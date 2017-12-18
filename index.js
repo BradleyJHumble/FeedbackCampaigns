59ead8019e4bed60650605552b6e671afd338b34
Bradley J. Humble <bradleyjhumble@gmail.com>
2017-12-17 19:04:52 -0600

    Google Stratergy

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
-passsport.use(new GoogleStrategy());
-
-
-
-
+passsport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		}, 
+		accessToken => {
+			console.log(accessToken);
+		}
+	)
+);
 
 // Heroku dynamic port
 const PORT = process.env.PORT || 5000;
