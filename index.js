58441fb06cf36dd1f078429eb98ee601e9028290
judikial <bradleyjhumble@gmail.com>
2017-12-31 18:16:46 -0600

    Enabling Cookies

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
 require('./models/User'); // needs to load before passport file tries to call it
 require('./services/passport'); // dont need to call it so only require
 const keys = require('./config/keys');
-mongoose.connect(keys.mongoURI);
 
-const app = express();
+mongoose.connect(keys.mongoURI); // db connect
+
+const app = express(); // express server through const app
+
+app.use( 
+	cookieSession({
+			maxAge: 30 * 24 * 60 * 60 * 1000, // Cookies age equals 30 days
+			keys: [keys.cookieKey]
+	});
+);
+
+// sets up passports system
+app.use(passport.initialize()); 
+app.use(passport.session());
+
 require('./routes/authRoutes')(app); // calling function with app object
-
 app.get('/', function(require, response) { // for testing 
 response.send("Hello World");
 });
 
-// Heroku dynamic port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // Heroku dynamic port
 app.listen(PORT);
