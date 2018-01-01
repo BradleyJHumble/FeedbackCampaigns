31ec7756cb565abcf9791ef4f51f452c4d0e888d
judikial <bradleyjhumble@gmail.com>
2017-12-31 22:09:21 -0600

    Try 4

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+	googleClientID: '75361341489-241d9tfto17baq4nnec7oesp5h744618.apps.googleusercontent.com',
+	googleClientSecret: 'Ew5HNo8cqmMeg0BFq2fMFf_t',
+	mongoURI: 'mongodb://Bradley:password@ds135757.mlab.com:35757/emaily-dev',
+	cookieKey: 'E6S/#9TjaV)d6?}?' // random key
+}; // cant see this on github
