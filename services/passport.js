e52bcf2122ee0b1ea96c27f2affeb86a0aaaaec7
judikial <bradleyjhumble@gmail.com>
2018-01-01 20:02:33 -0600

    Fixed Heroku Proxy Errors in GoogleStrategy

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,8 @@ passport.use(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
+			callbackURL: '/auth/google/callback',
+			proxy: true // for Heroku proxy 
 		}, 
 		(accessToken, refreshToken, profile, done) => { 
 			// findOne sees if user has an account
