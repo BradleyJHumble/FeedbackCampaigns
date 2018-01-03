dc573cde55e3053c923b79e21b4bc530ed3ff58b
judikial <bradleyjhumble@gmail.com>
2018-01-02 19:46:36 -0600

    Added Materialize-CSS for the Client

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 // top level index file
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
