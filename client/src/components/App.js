304d5001dfde8ee3de65715dbe94f6ed7d1eb1f5
judikial <bradleyjhumble@gmail.com>
2018-01-04 16:52:48 -0600

    Moved Footer to only be on Landing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,7 @@ import * as actions from '../actions';
 // importing components form their files
 import Header from './Header';
 import Landing from './Landing';
-import Footer from './Footer';
+
 
 const Dashboard = () => <h2> Dashboard </h2>
 const SurveryNew = () => <h2> SurveryNew </h2>
@@ -28,16 +28,7 @@ class App extends Component {
 			  	<Route exact path="/" component={Landing} />
 			  	<Route exact path="/surveys" component={Dashboard} />
 			  	<Route exact path="/surveys/new" component={SurveryNew} />
-			  	<div className=" grey lighten-3  black-text center-align page-footer" 
-			  	style={{
-			  		height: '10%',
-			  		position: 'absolute',
-  					right: '0',
-  					bottom: '0',
-  					left: '0',
-  					padding: '1rem' }}>
-			  	<Footer  />
-			  	</div>
+			  	
 			  	</div>
 			  </div>
 			  </BrowserRouter>
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import Footer from './Footer';
 
 const Landing =() => {
 	return (
 			<div style={{ textAlign:'center'}}>
 			<h1> FeedbackCampaigns</h1>
 			<h3> Collect feedback from your users</h3>
+			<div className=" grey lighten-3  black-text center-align page-footer" 
+			  	style={{
+			  		height: '10%',
+			  		position: 'absolute',
+  					right: '0',
+  					bottom: '0',
+  					left: '0',
+  					padding: '1rem' }}>
+			  	<Footer />
+			  	</div>
+		
 			</div>
 		);
 };
