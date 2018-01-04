0fc5d26b7aed40b0b16b0def21b58ddae95331c4
judikial <bradleyjhumble@gmail.com>
2018-01-03 20:12:34 -0600

    Testing fetchUser

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-const fetchUser = (dispatch) => {
-	return function() {
+export const fetchUser = () => {
+	return function(dispatch) {
 		axios
 			.get('/api/current_user')
 			.then(res => dispatch({ type: FETCH_USER, payload: res}));
