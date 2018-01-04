1c37ced3610999aba0f0be692a6a6ddfafc55a34
judikial <bradleyjhumble@gmail.com>
2018-01-04 03:13:50 -0600

    Fixing Link errors

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,7 +10,7 @@ class Footer extends Component {
 		return ( <div > <div>
 
 		  <div className="row">
-    <div className="col s12 m6 l3"><p>About Us</p></div>
+    <div className="col s12 m6 l3"><Link to={'/about-us'} > <p>About Us</p> </Link></div> 
     <div className="col s12 m6 l3"><p>Terms of Use</p></div>
     <div className="col s12 m6 l3"><p>Careers</p></div>
     <div className="col s12 m6 l3"><p>Contact Us</p></div>
