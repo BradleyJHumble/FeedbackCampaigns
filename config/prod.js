18da337c89dc4e7e6481d4ec29228f416fb85b73
judikial <bradleyjhumble@gmail.com>
2018-01-06 17:34:28 -0600

    Stripe Keys SetUp

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,7 @@ module.exports = {
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	mongoURI: process.env.MONGO_URI,
-	cookieKey: process.env.COOKIE_KEY // random key
+	cookieKey: process.env.COOKIE_KEY, // random key
+	stripePblishableKey: process.env.STRIPE_PUBLISHABLE_KEY, // stripe front end key
+	stripeSecretKey: process.env.STRIPE_SECRET_KEY // stripe back end key
 }; 
