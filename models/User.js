3b07958e59bf0f183e4e011fd2320de53ff116e7
judikial <bradleyjhumble@gmail.com>
2018-01-14 05:42:31 -0600

    Credit System through the Passport Oauth Token

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema ({
 	googleId: String,
-	facebookId: String
+	facebookId: String,
+	credits: { type: Number, default: 25}
 });
 
 mongoose.model('users', userSchema); // parsing userSchema as an model through mongoose
