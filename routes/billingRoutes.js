98f6552b586c0adcc221fa9c11b09125cf789730
judikial <bradleyjhumble@gmail.com>
2018-01-14 06:23:33 -0600

    Route Specific Middlewears for Stripe Billing + Credit switch to charging

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,20 +1,18 @@
 const keys = require('../config/keys');
-
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-	app.post('/api/stripe', async (req, res) => {
-		if (!req.user) {
-			return res.status(401).send({ error: 'You must log in!'});
-		}
-
+	app.post('/api/stripe', requireLogin, async (req, res) => {
+ if (req.user.credits = 0) {
 		const charge = await stripe.charges.create({
 			amount: 500,
 			currency: 'usd',
-			description: '$5 for 5 credits',
+			description: '$5 for an Email Campaign',
 			source: req.body.id,
 		});
-		req.user.credits += 5;
+ }
+		req.user.credits - 1;
 		const user = await req.user.save();
 		res.send(user);
 	});
