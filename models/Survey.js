cb08ec0e00cef712e0f576e273a8ac57f8a26469
judikial <bradleyjhumble@gmail.com>
2018-01-21 17:29:33 -0600

    Added to Survey Schema

--- models/Survey.js
+++ models/Survey.js
@@ -8,7 +8,10 @@ const surveySchema = new Schema({
 	subject: String,
 	recipients: [RecipientSchema],
 	yes: { type: Number, default: 0},
-	no: { type: Number, default: 0}
+	no: { type: Number, default: 0},
+	_user: { type: Schema.Types.ObjectId, ref: 'User'}, // user tagging to each survey
+	dateSent: Date,
+	lastResponsed: Date
 });
 
 mongoose.model('surveys', surveySchema);
