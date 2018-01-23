6f45933adacfcf7bc67edd093dbd39e39347b6a6
judikial <bradleyjhumble@gmail.com>
2018-01-22 20:42:50 -0600

    Mailer Constructor + surveyTemplate error fixed

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,4 +1,4 @@
-module exports = (survey) => {
+module.exports = (survey) => {
 
 	return '<div>' + survey.body + '</div>';
 
--- services/mailer.js
+++ services/mailer.js
@@ -6,6 +6,16 @@ const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
 
+	constructor({ subject, recipients}, content){
+		super(); // es2015 syntax
+
+		this.from_email = new helper.Email('no-reply@feedbackcampaigns.com');
+		this.subject = subject;
+		this.body = new helper.Content('text/html', content);
+		this.recipients = this.formatAddresses(recipients);
+
+	}
+
 }
 
 module.exports = Mailer;
