e0f394b2bd946002610cbdc6e67d9dd24ad0ed34
judikial <bradleyjhumble@gmail.com>
2018-01-22 23:42:34 -0600

    Major Reform-- I am very upset

--- services/mailer.js
+++ services/mailer.js
@@ -1,60 +1,55 @@
-// file is capitalized to fit naming conventions of exporting an class
 const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
-
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
 
-	constructor({ subject, recipients}, content){
-		super(); // es2015 syntax
+    this.sgApi = sendgrid(keys.sendGridKey);
+    this.from_email = new helper.Email('no-reply@emaily.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
 
-		this.sgAPI = sendgrid(keys.sendGridKey);
-		this.from_email = new helper.Email('no-reply@feedbackcampaigns.com');
-		this.subject = subject;
-		this.body = new helper.Content('text/html', content);
-		this.recipients = this.formatAddresses(recipients);
-		
-		this.addContent(this.body); // built in helper function from sendgrid
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
 
-		this.addClickTracking();
-		this.addRecipients();
-	}
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
 
-	formatAddresses(recipients) { // this is an helper fuction to deformat the emails from the array the Mailer Constructor recieves
-		return recipients.map(({ email }) => {
-			return new helper.Email(email);
-		});
-	}
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
 
-	addClickTracking() {
-		const trackingSettings = new helper.TrackingSettings();
-		const clickTracking = new helper.ClickTracking(true, true);
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
 
-		trackingSettings.setClickTracking(clickTracking);
-		this.addTrackingSettings(trackingSettings);
-	}
+  addRecipients() {
+    const personalize = new helper.Personalization();
 
-	addRecipients(){
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
 
-		const personalize = new helper.Personalization();
-		this.recipients.forEach(recipient => {
-			personalize.addTo(recipient);
-		});
-		this.addPersonalization(personalize);
-	}
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
 
-	async send() {
-		const request = this.sgAPI.emptyRequest({ // sendgrid's library is ugly...
-			method: 'POST',
-			path: '/v3/mail/send',
-			body: this.toJSON()
-		});
-
-		const response = this.sgAPI.API(request); // actual sender using sendgrids api helper (API)
-		return response;
-
-	}
+    const response = await this.sgApi.API(request);
+    return response;
+  }
 }
 
 module.exports = Mailer;
